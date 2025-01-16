8f2d5db4defdb29088439d273ee4ae290a99c86e
felipeselau <felipeselau68@gmail.com>
2025-01-16 15:26:59 -0300

    added singleFetch future flag

--- vite.config.ts
+++ vite.config.ts
@@ -55,7 +55,7 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         v3_lazyRouteDiscovery: true,
-        v3_singleFetch: false,
+        v3_singleFetch: true,
         v3_routeConfig: true,
       },
     }),
